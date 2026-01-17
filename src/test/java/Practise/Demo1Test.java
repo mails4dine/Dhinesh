b166d49022b77e1cee6d5bda0f2d4d1d7d94aa55
mails4dine <mails4dine@gmail.com>
2026-01-18 00:48:05 +0530

    test

---
+++ src/test/java/Practise/Demo1Test.java
@@ -0,0 +1,13 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups ="regression")
+	public void MangoTest() {
+	
+		System.out.println("Hi Mango Fruit");
+		System.out.println("Hi NewMango Fruit");
+	}
+}
---
+++ src/test/java/Practise/DemoTest.java
@@ -0,0 +1,10 @@
+package Practise;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test(groups = "smoke")
+	public void appleTest() {
+		
+		System.out.println("Hi Apple Fruit");
+	}
+}
